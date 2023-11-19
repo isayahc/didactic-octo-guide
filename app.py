b66fdacf021e537436a3a83ebda86e603e1d8691
Isayah Culbertson <isayahCulbertson@gmail.com>
2023-11-19 18:23:52 +0000

    modify chat to stream one token at a time

--- app.py
+++ app.py
@@ -9,17 +9,23 @@ model = AutoModelForCausalLM.from_pretrained("gpt2")
 def stream_response(input_text: str):
     # Encode the input text
     input_ids = tok.encode(input_text, return_tensors="pt")
-    
-    # Generate response in a streaming fashion
-    chatbot_output = model.generate(
-        input_ids,
-        max_length=1000,
-        pad_token_id=tok.eos_token_id,
-        do_sample=True
-    )
-    
-    response_text = tok.decode(chatbot_output[:, input_ids.shape[-1]:][0], skip_special_tokens=True)
-    return response_text
+
+    # Generate one token at a time and yield it
+    for _ in range(50):  # Limiting to 50 tokens to prevent too long generation
+        # Generate one token
+        chatbot_output = model.generate(
+            input_ids,
+            max_new_tokens=1,  # Generate one token at a time
+            pad_token_id=tok.eos_token_id,
+            do_sample=True,
+            temperature=0.7  # Sampling temperature, adjust as needed
+        )
+        # Extract the last token
+        last_token = chatbot_output[:, -1].tolist()[0]
+        # Update the input_ids
+        input_ids = torch.cat((input_ids, torch.tensor([[last_token]])), dim=1)
+        # Decode and yield the token
+        yield tok.decode([last_token], skip_special_tokens=True)
 
 with gr.Blocks() as demo:
     chatbot = gr.Chatbot()
@@ -31,14 +37,13 @@ with gr.Blocks() as demo:
 
     def bot(history: list):
         input_text = ' '.join([h[0] for h in history if h[0]])  # Join all user messages
-        response = stream_response(input_text)
-        for char in response:
+        # Call stream_response which yields one token at a time
+        for char in stream_response(input_text):
             if history[-1][1] is None:
                 history[-1][1] = char
             else:
                 history[-1][1] += char
             yield history
-            # Use time.sleep(0.1) if you want to simulate typing delay
 
     msg.submit(user, inputs=[msg, chatbot], outputs=[msg, chatbot]).then(
         bot, inputs=chatbot, outputs=chatbot
